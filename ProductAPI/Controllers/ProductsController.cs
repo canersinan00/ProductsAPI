f0f8b204c05a35a286c4975b411f08d6db36045d
canersinan00 <canersinan1013@gmail.com>
2025-10-18 13:35:09 +0300

    Update Products Controller Methods

--- ProductAPI/Controllers/ProductsController.cs
+++ ProductAPI/Controllers/ProductsController.cs
@@ -24,17 +24,31 @@ namespace ProductsAPI.Controllers
         }
         //localhost:5000/api/products => GET
         [HttpGet]
-        public List<Product> GetProducts()
+        public IActionResult GetProducts()
         {
-            return _products ?? new List<Product>();
+            if (_products == null)
+            {
+                return NotFound();
+            }
+            return Ok(_products);
         }
 
         //localhost:5000/api/products/1 => GET
 
         [HttpGet("{id}")]
-        public Product GetProduct(int id)
+        public IActionResult GetProduct(int? id)
         {
-            return _products?.FirstOrDefault(p => p.ProductId == id) ?? new Product();
+            if (id == null)
+            {
+                return NotFound();
+            }
+            var p = _products?.FirstOrDefault(p => p.ProductId == id);
+
+            if (p == null)
+            {
+                return NotFound();
+            }
+            return Ok(p);
             
         }
         
