31ba8027481ef931851418246efc3acb326b3e79
canersinan00 <canersinan1013@gmail.com>
2025-10-19 11:35:51 +0300

    Generate JWT

--- ProductAPI/Controllers/UsersController.cs
+++ ProductAPI/Controllers/UsersController.cs
@@ -1,5 +1,9 @@
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
 using ProductAPI.Models;
 using ProductsAPI.DTO;
 
@@ -9,10 +13,15 @@ namespace ProductsAPI.Controllers
     [Route("api/[controller]")]
     public class UsersController : ControllerBase
     {
-        private UserManager<AppUser> _userManager;
-        public UsersController(UserManager<AppUser> userManager)
+        private readonly UserManager<AppUser> _userManager;
+        private readonly SignInManager<AppUser> _signInManager;
+        private readonly IConfiguration _configuration;
+
+        public UsersController(UserManager<AppUser> userManager,SignInManager<AppUser> signInManager,IConfiguration configuration)
         {
             _userManager = userManager;
+            _signInManager = signInManager;
+            _configuration = configuration;
         }
 
         [HttpPost("register")]
@@ -20,14 +29,15 @@ namespace ProductsAPI.Controllers
         {
             if (!ModelState.IsValid)
             {
-                    return BadRequest(ModelState);
+                return BadRequest(ModelState);
             }
 
-             var user = new AppUser
+            var user = new AppUser
             {
                 UserName = model.UserName,
                 Email = model.Email,
-                FullName = model.FullName,                    DateAdded = DateTime.Now
+                FullName = model.FullName,
+                DateAdded = DateTime.Now
             };
 
             var result = await _userManager.CreateAsync(user, model.Password);
@@ -35,8 +45,45 @@ namespace ProductsAPI.Controllers
             if (result.Succeeded)
             {
                 return StatusCode(201);
-            }         
-            return BadRequest(result.Errors);   
+            }
+            return BadRequest(result.Errors);
+        }
+        
+        [HttpPost("login")]
+        public async Task<IActionResult> Login(LoginDTO model)
+        {
+            var user = await _userManager.FindByEmailAsync(model.Email);
+            if (user == null)
+            {
+                return BadRequest(new { message = "Invalid Email or Password" });
+            }
+            var result = await _signInManager.CheckPasswordSignInAsync(user, model.Password, false);
+
+            if (result.Succeeded)
+            {
+                return Ok(new { token = GenerateJWT(user) });
+            }
+            return Unauthorized();
+        }
+
+        private object GenerateJWT(AppUser user)
+        {
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var key = Encoding.ASCII.GetBytes(_configuration.GetSection("AppSettings:Secret").Value ?? "");
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Subject = new ClaimsIdentity(
+                    new Claim[]
+                    {
+                        new Claim(ClaimTypes.NameIdentifier, user.Id.ToString()),
+                        new Claim(ClaimTypes.Name, user.UserName ?? ""),
+                    }
+                ),
+                Expires = DateTime.UtcNow.AddDays(1),
+                SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256Signature)
+            };
+            var token = tokenHandler.CreateToken(tokenDescriptor);
+            return tokenHandler.WriteToken(token);
         }
     }
 }
